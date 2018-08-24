47602accb2032ff8e7ecf30e4f17b2b0b051f964
zwq-qianyu <1347638091@qq.com>
2018-08-24 14:43:02 +0800

    Initial Commit

---
+++ Weapons/Weapon.swift
@@ -0,0 +1,16 @@
+//
+//  Weapon.swift
+//  Weapons
+//
+//  Created by 梓逸宸 on 2018/8/20.
+//  Copyright © 2018年 梓逸宸. All rights reserved.
+//
+
+struct Weapon {
+    var name = ""
+    var type = ""
+    var image = ""
+    
+    //整体介绍
+    var text = ""
+}
